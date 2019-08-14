efb7e5e02f08905e592d1c73df68661a4ae6cd3d
Kit Burton-Senior <mail@kitbs.com>
2019-08-14 08:55:48 +0100

    create gb bank rule

---
+++ library/Rules/Locale/GbBank.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules\Locale;
+
+use InvalidArgumentException;
+
+use Cs278\BankModulus\SortCode;
+use Cs278\BankModulus\Exception\SortCodeInvalidException;
+use Respect\Validation\Rules\Bank;
+
+/**
+ * Validates a United Kingdom bank.
+ *
+ * This validator depends on the composer package "cs278/bank-modulus".
+ *
+ * @author Chris Smith <chris@cs278.org>
+ *
+ * @see    SortCode::create()
+ */
+class GbBank extends Bank
+{
+    /**
+     * @return bool
+     */
+    public function validate($input)
+    {
+        try {
+            $sortCode = SortCode::create($input);
+        }
+        catch (SortCodeInvalidException $e) {
+            return false;
+        }
+        catch (InvalidArgumentException $e) {
+            return false;
+        }
+
+        return true;
+    }
+}
